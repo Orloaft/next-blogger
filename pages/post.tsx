98480aa20266027c2b08362beef7505684d48b87
unknown <alexorlow17@gmail.com>
2023-07-13 17:20:34 -0400

    add restyled editor

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,10 +1,33 @@
 import PostEditor from "@/components/PostEditor/PostEditor";
-import { Layout } from "@/components/layout";
+import React, { useState } from "react";
 
-export default function PostPage() {
+const CreatePostPage: React.FC = () => {
+  const [content, setContent] = useState("");
+
+  const handleSave = (updatedContent: string) => {
+    setContent(updatedContent);
+  };
+
+  const handlePublish = () => {
+    // Perform save or publish actions with the content
+  };
+
   return (
-    <Layout>
-      <PostEditor />
-    </Layout>
+    <>
+      <div
+        style={{
+          border: " 2px solid black",
+          borderRadius: ".5rem",
+          padding: "1rem",
+          maxWidth: "40vw",
+          minHeight: "50vh",
+        }}
+      >
+        <PostEditor />
+      </div>{" "}
+      <button onClick={handlePublish}>Publish</button>
+    </>
   );
-}
+};
+
+export default CreatePostPage;
