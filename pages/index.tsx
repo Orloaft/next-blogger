c8c6300303dfb36476c4f5cfa84dedce19bce25d
unknown <alexorlow17@gmail.com>
2023-07-18 15:12:44 -0400

    change path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { Layout } from "@/components/Layout";
+import { Layout } from "../components/Layout";
 import { JSONContent } from "@tiptap/react";
 
 import SignIn from "@/components/Auth/AccountInfo";
